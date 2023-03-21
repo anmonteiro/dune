4376bb72f50f0fde60d6df3e569a5ea271263bdf
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-03-20 23:54:09 -0700

    refactor: only traverse build_dirs if they're melange target dirs
    Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- src/dune_rules/alias_rec.ml
+++ src/dune_rules/alias_rec.ml
@@ -19,36 +19,7 @@ module Alias_status = struct
   include Monoid.Make (T)
 end
 
-module Source_tree_only = struct
-  let dep_on_alias_if_exists alias =
-    let open Action_builder.O in
-    let* definition = Action_builder.of_memo (Load_rules.alias_exists alias) in
-    match definition with
-    | false -> Action_builder.return Alias_status.Not_defined
-    | true ->
-      let+ () = Action_builder.alias alias in
-      Alias_status.Defined
-
-  module Map_reduce =
-    Source_tree.Dir.Make_map_reduce (Action_builder) (Alias_status)
-
-  let dep_on_alias_rec name dir =
-    let ctx_name, src_dir = Path.Build.extract_build_context_exn dir in
-    let build_dir = Context_name.build_dir (Context_name.of_string ctx_name) in
-    let f dir =
-      let path =
-        Path.Build.append_source build_dir (Source_tree.Dir.path dir)
-      in
-      dep_on_alias_if_exists (Alias.make ~dir:path name)
-    in
-    let open Action_builder.O in
-    Source_tree.find_dir src_dir |> Action_builder.of_memo >>= function
-    | None -> Action_builder.return Alias_status.Not_defined
-    | Some src_dir ->
-      Map_reduce.map_reduce src_dir ~traverse:Sub_dirs.Status.Set.normal_only ~f
-end
-
-module Traverse_build_dirs = struct
+module In_melange_target_dir = struct
   module Lookup_alias = struct
     type result =
       { alias_exists : Alias_status.t
@@ -66,20 +37,18 @@ module Traverse_build_dirs = struct
 
   let dep_on_alias_if_exists alias =
     let open Action_builder.O in
-    let* load_rules_result =
-      Action_builder.of_memo
-        (Load_rules.load_dir ~dir:(Path.build (Alias.dir alias)))
-    in
-    match load_rules_result with
+    Action_builder.of_memo
+      (Load_rules.load_dir ~dir:(Path.build (Alias.dir alias)))
+    >>= function
     | Source _ | External _ ->
       Code_error.raise "Alias in a non-build dir"
         [ ("alias", Alias.to_dyn alias) ]
     | Build { aliases; allowed_subdirs; rules_here = _ } -> (
-      match Alias.Name.Map.find aliases (Alias.name alias) with
-      | None ->
+      match Alias.Name.Map.mem aliases (Alias.name alias) with
+      | false ->
         Action_builder.return
           (Lookup_alias.of_dir_set ~exists:Not_defined allowed_subdirs)
-      | Some _ ->
+      | true ->
         Action_builder.alias alias
         >>> Action_builder.return
               (Lookup_alias.of_dir_set ~exists:Defined allowed_subdirs))
@@ -92,31 +61,69 @@ module Traverse_build_dirs = struct
   let dep_on_alias_rec name dir =
     let rec map_reduce dir ~f =
       let open Action_builder.O in
-      let* should_traverse =
-        match Path.Build.drop_build_context dir with
-        | None -> Action_builder.return true
-        | Some src_dir -> (
-          Action_builder.of_memo (Source_tree.find_dir src_dir) >>| function
-          | Some src_dir -> (
-            match Source_tree.Dir.status src_dir with
-            | Normal -> true
-            | Vendored | Data_only -> false)
-          | None -> true)
-      in
-      match should_traverse with
-      | false -> Action_builder.return Alias_status.empty
-      | true ->
-        let* { Lookup_alias.alias_exists; allowed_subdirs } = f dir in
-        Action_builder.List.fold_left (String.Set.to_list allowed_subdirs)
-          ~init:alias_exists ~f:(fun alias_exists s ->
-            let+ alias_exists' = map_reduce (Path.Build.relative dir s) ~f in
-            Alias_status.combine alias_exists alias_exists')
+      let* { Lookup_alias.alias_exists; allowed_subdirs } = f dir in
+      Action_builder.List.fold_left (String.Set.to_list allowed_subdirs)
+        ~init:alias_exists ~f:(fun alias_exists s ->
+          let+ alias_exists' = map_reduce (Path.Build.relative dir s) ~f in
+          Alias_status.combine alias_exists alias_exists')
     in
     map_reduce dir ~f:(fun dir -> dep_on_alias_if_exists (Alias.make ~dir name))
 end
 
-let dep_on_alias_rec ~project alias dir =
-  let dune_version = Dune_project.dune_version project in
-  match Dune_lang.Syntax.Version.Infix.(dune_version >= (3, 8)) with
-  | true -> Traverse_build_dirs.dep_on_alias_rec alias dir
-  | false -> Source_tree_only.dep_on_alias_rec alias dir
+let rec map_reduce ~build_dir ~name t ~f =
+  match
+    Sub_dirs.Status.(Map.find Set.normal_only (Source_tree.Dir.status t))
+  with
+  | false -> Action_builder.return Alias_status.empty
+  | true -> (
+    let open Action_builder.O in
+    let* found_in_source =
+      let+ here = f t
+      and+ in_sub_dirs =
+        Action_builder.List.map
+          (Filename.Map.values (Source_tree.Dir.sub_dirs t))
+          ~f:(fun s ->
+            let* t = Action_builder.of_memo (Source_tree.Dir.sub_dir_as_t s) in
+            map_reduce ~build_dir ~name t ~f)
+      in
+      List.fold_left in_sub_dirs ~init:here ~f:Alias_status.combine
+    in
+    let build_path =
+      Path.Build.append_source build_dir (Source_tree.Dir.path t)
+    in
+    Action_builder.of_memo (Only_packages.stanzas_in_dir build_path)
+    >>= function
+    | None -> Action_builder.return found_in_source
+    | Some stanzas ->
+      let+ in_melange_target_dirs =
+        let melange_target_dirs =
+          List.filter_map stanzas.stanzas ~f:(function
+            | Melange_stanzas.Emit.T mel ->
+              Some (Melange_stanzas.Emit.target_dir ~dir:build_path mel)
+            | _ -> None)
+        in
+        Action_builder.List.map melange_target_dirs ~f:(fun s ->
+            In_melange_target_dir.dep_on_alias_rec name s)
+      in
+      List.fold_left in_melange_target_dirs ~init:found_in_source
+        ~f:Alias_status.combine)
+
+let dep_on_alias_if_exists alias =
+  let open Action_builder.O in
+  Action_builder.of_memo (Load_rules.alias_exists alias) >>= function
+  | false -> Action_builder.return Alias_status.Not_defined
+  | true ->
+    let+ () = Action_builder.alias alias in
+    Alias_status.Defined
+
+let dep_on_alias_rec name dir =
+  let ctx_name, src_dir = Path.Build.extract_build_context_exn dir in
+  let build_dir = Context_name.build_dir (Context_name.of_string ctx_name) in
+  let f dir =
+    let path = Path.Build.append_source build_dir (Source_tree.Dir.path dir) in
+    dep_on_alias_if_exists (Alias.make ~dir:path name)
+  in
+  let open Action_builder.O in
+  Source_tree.find_dir src_dir |> Action_builder.of_memo >>= function
+  | None -> Action_builder.return Alias_status.Not_defined
+  | Some src_dir -> map_reduce ~build_dir ~name src_dir ~f
