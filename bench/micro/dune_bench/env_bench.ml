69c51652fc704aad3d75a8961ff1c0a3cb3c57fc
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-08-02 21:21:40 -0700

    bench: cover absent temporary directory variable

--- bench/micro/dune_bench/env_bench.ml
+++ bench/micro/dune_bench/env_bench.ml
@@ -1,17 +1,33 @@
 open Stdune
 
-let make_env size =
-  List.init size ~f:(fun i ->
-    let prefix = if i mod 2 = 0 then 'A' else 'Z' in
-    Printf.sprintf "%c_DUNE_BENCH_%04d=value-%04d" prefix i i)
-  |> Array.of_list
-  |> Env.of_unix
-  |> Env.add ~var:Env.Var.temp_dir ~value:"original-temp-directory"
+let make_env size ~temp_dir =
+  let env =
+    List.init size ~f:(fun i ->
+      let prefix = if i mod 2 = 0 then 'A' else 'Z' in
+      Printf.sprintf "%c_DUNE_BENCH_%04d=value-%04d" prefix i i)
+    |> Array.of_list
+    |> Env.of_unix
+  in
+  if temp_dir
+  then Env.add env ~var:Env.Var.temp_dir ~value:"original-temp-directory"
+  else env
 ;;
 
-let%bench_fun ("render environment override" [@indexed variables = [ 10; 100; 1_000 ]]) =
-  let env = make_env variables in
+let render_environment_override ~temp_dir variables =
+  let env = make_env variables ~temp_dir in
   fun () ->
     Env.to_unix_with_override env ~var:Env.Var.temp_dir ~value:"dune-temp-directory"
     |> ignore
+;;
+
+let%bench_fun
+    ("render environment override: present" [@indexed variables = [ 10; 100; 1_000 ]])
+  =
+  render_environment_override ~temp_dir:true variables
+;;
+
+let%bench_fun
+    ("render environment override: absent" [@indexed variables = [ 10; 100; 1_000 ]])
+  =
+  render_environment_override ~temp_dir:false variables
 ;;
